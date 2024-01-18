58ef41e6983538ca810007479d59a61df444930a
Ryan Ruckley <rruckley@gmail.com>
2024-01-19 10:31:01 +1100

    consts

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Metro Ethernet Forum API Library
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[warn(missing_docs)]
+
+const LIB_PATH : &str = "mef-api";
+
