0612e4df096be96dab00afa60046564a7061db20
Ryan Ruckley <rruckley@gmail.com>
2024-01-19 13:53:30 +1100

    more fields

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -2,13 +2,18 @@
 //! 
 
 
-use tmflib::tmf674::geographic_site::GeographicSite as TMFSite;
+use tmflib::{tmf674::geographic_site::GeographicSite as TMFSite, HasId};
 use crate::w122::geographic_site::GeographicSite as MEFSite;
 use std::convert::From;
 
 impl From<MEFSite> for TMFSite {
     fn from(value: MEFSite) -> Self {
-        let site = TMFSite::new(value.name);
+        let mut site = TMFSite::new(value.name);
+        site.description = Some(value.description.clone());
+        site.
+        // Copying the id across might violate uniqueness constraints
+        site.id = Some(value.id.clone());
+        site.generate_href();
         site
     }
     
