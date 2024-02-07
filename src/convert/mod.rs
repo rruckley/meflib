6efad0d043285d057c5173a53c61c35b6bee20e7
Ryan Ruckley <rruckley@gmail.com>
2024-02-08 10:11:56 +1100

    enhanced

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -3,6 +3,9 @@
 
 
 use tmflib::{tmf674::geographic_site::GeographicSite as TMFSite, HasId};
+use tmflib::tmf629::customer::Customer;
+use tmflib::tmf632::organization::Organization;
+use tmflib::common::related_party::RelatedParty;
 use crate::w122::geographic_site::GeographicSite as MEFSite;
 use std::convert::From;
 
@@ -10,10 +13,12 @@ impl From<MEFSite> for TMFSite {
     fn from(value: MEFSite) -> Self {
         let mut site = TMFSite::new(value.name);
         site.description = Some(value.description.clone());
-        site.
         // Copying the id across might violate uniqueness constraints
         site.id = Some(value.id.clone());
         site.generate_href();
+        let org = Organization::from(value.customer_name);
+        let party = RelatedParty::from(org);
+        // site.related_party.push(party)
         site
     }
     
