a287f97e2409d5c4e25ad4bb0ae5eee47cdd1494
Ryan Ruckley <rruckley@gmail.com>
2024-02-08 10:33:32 +1100

    add customer details

--- examples/mef2tmf_site.rs
+++ examples/mef2tmf_site.rs
@@ -1,12 +1,13 @@
 //! Convert from MEF to TMF Site object
 use meflib::w122::geographic_site::GeographicSite as MEFSite;
-use tmflib::tmf674::geographic_site::GeographicSite as TMFSite;
+use tmflib::tmf674::geographic_site_v4::GeographicSite as TMFSite;
 
 fn main() {
     let mef = MEFSite {
         id : "mef123".to_string(),
         name: "MyMEFSite".into(),
         description : "This is a MEF site to be converted to TMF".into(),
+        customer_name : "Orange Computers".into(),
         ..Default::default()
     };
 
