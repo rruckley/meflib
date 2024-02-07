63584b168b109aafb87ed03b47ef0d17706546d8
Ryan Ruckley <rruckley@gmail.com>
2024-02-08 10:11:49 +1100

    Example conversion

---
+++ examples/mef2tmf_site.rs
@@ -0,0 +1,16 @@
+//! Convert from MEF to TMF Site object
+use meflib::w122::geographic_site::GeographicSite as MEFSite;
+use tmflib::tmf674::geographic_site::GeographicSite as TMFSite;
+
+fn main() {
+    let mef = MEFSite {
+        id : "mef123".to_string(),
+        name: "MyMEFSite".into(),
+        description : "This is a MEF site to be converted to TMF".into(),
+        ..Default::default()
+    };
+
+    let tmf : TMFSite = TMFSite::from(mef);
+
+    dbg!(tmf);
+}
